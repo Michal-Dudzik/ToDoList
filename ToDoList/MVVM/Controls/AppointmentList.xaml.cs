995e4ded2862dead4fea871be1f6ef70314c07f2
Michal-Dudzik <dumichal20@wp.pl>
2022-06-07 01:46:29 +0200

    PatientView działa i wygląda ok, tylko usuwanie z bazy coś nie wychodzi, poprawić i dokończyć

---
+++ ToDoList/MVVM/Controls/AppointmentList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ToDoList.MVVM.Controls
+{
+    public partial class AppointmentList : UserControl
+    {
+        public AppointmentList()
+        {
+            InitializeComponent();
+        }
+    }
+}
